659b5682ed48301dc9a438aefb0dbf0345f08303
Sharz Coder <sibudasharon@gmail.com>
2022-09-08 10:47:37 -0700

    C print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- *  main - Entry point
+ *  main - print the string in the put function
  *
  *  Description: using the main function
  *  this program prints "Programming is like building a multilingual puzzle
